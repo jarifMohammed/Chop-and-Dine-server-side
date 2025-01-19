5a0c43812f36bb5d408817fc08cb384e8409f4d8
jarifMohammed <mohammedjarif@protonmail.com>
2025-01-20 00:24:45 +0600

    Admin secured multi layer

--- index.js
+++ index.js
@@ -64,12 +64,28 @@ async function run() {
       })
       res.send({token})
     })
-    // user related api
-    app.get('/users',verifyToken,async (req,res) => {
+    
+
+// user verify adin after verify token
+ const verifyAdmin = async ( req,res,next) => {
+  const email = req.decoded.email
+  const query = {email:email}
+  const user = await userCollection.findOne(query)
+  const isAdmin = user?.role === 'admin'
+  if(!isAdmin){
+    return res.status(403).send({ message : 'forbidden access'})
+  }
+  next()
+ }
+
+
+// user related api
+app.get('/users',verifyToken,verifyAdmin,async (req,res) => {
      
-      const result = await userCollection.find().toArray()
-      res.send(result)
-    })
+  const result = await userCollection.find().toArray()
+  res.send(result)
+})
+
     // cheking is admin
     app.get('/users/admin/:email', verifyToken, async (req,res)=> {
       const email = req.params.email
@@ -97,13 +113,13 @@ async function run() {
       const result = await userCollection.insertOne(user)
       res.send(result)
     })
-    app.delete('/users/:id' , async (req,res) => {
+    app.delete('/users/:id' ,verifyToken,verifyToken, async (req,res) => {
       const id = req.params.id
       const query = {_id : new ObjectId(id)}
       const result = await userCollection.deleteOne(query)
       res.send(result)
     })
-    app.patch('/users/admin/:id' , async(req,res) => {
+    app.patch('/users/admin/:id',verifyToken,verifyAdmin , async(req,res) => {
       const id = req.params.id
       const filter = { _id : new ObjectId(id)}
       const updatedRole ={
